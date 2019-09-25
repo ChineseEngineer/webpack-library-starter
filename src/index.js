ad4cb222bcb9a93e872460b1e1d2d1a6215ff76e
FE-tianyonggang <tianyonggang@aibao.com>
2019-09-25 17:49:37 +0800

    :tada: 初始化提交

--- src/index.js
+++ src/index.js
@@ -1,3 +1,92 @@
-import Cat from './cat.js';
-import Dog from './dog.js';
-export { Dog, Cat };
+import dsbridge from 'dsbridge'
+
+const invokeDsBridge = (name, params = {}) => {
+  if (!dsbridge) {
+    window.alert('no supported')
+  } else {
+    return dsbridge.call(name, params, (res) => {
+      if (params.success && typeof params.success === 'function') {
+        res = JSON.parse(res)
+        return params.success(res)
+      }
+    })
+  }
+}
+
+export default {
+  /**
+   * 分享
+   * @param params
+   * @return {undefined}
+   */
+  share: (params) => {
+    return invokeDsBridge('share', params)
+  },
+  /**
+   * 关闭webview
+   * @param params
+   * @return {undefined}
+   */
+  close: (params) => {
+    return invokeDsBridge('close', params)
+  },
+  /**
+   * 拨打电话
+   * @param params
+   * @return {undefined}
+   */
+  telephone: (params) => {
+    return invokeDsBridge('telephone', params)
+  },
+  /**
+   * 唤起原生登录
+   * @param params
+   * @return {undefined}
+   */
+  login: (params) => {
+    return invokeDsBridge('login', params)
+  },
+  /**
+   * 唤起地图导航
+   * @param params
+   * @return {undefined}
+   */
+  mapNavi: (params) => {
+    return invokeDsBridge('mapNavi', params)
+  },
+  /**
+   * 支付成功调用 通知原生
+   * @param params
+   * @return {undefined}
+   */
+  paySuccess: (params) => {
+    return invokeDsBridge('paySuccess', params)
+  },
+  /**
+   * 获取app版本信息
+   * @param params
+   * @return {undefined}
+   */
+  getAppVersion: (params) => {
+    return invokeDsBridge('getAppVersion', params)
+  },
+  /**
+   * 打开原生页面或新的webview 可控制原生头显示类型
+   * @param params
+   * @return {undefined}
+   */
+  newAction: (params) => {
+    return invokeDsBridge('newAction', params)
+  },
+  /**
+   * 校验相册权限
+   * @param params
+   * @return {undefined}
+   */
+  isHaveCameraPermission: (params) => {
+    return invokeDsBridge('isHaveCameraPermission', params)
+  },
+  wxNativePay: (params) => {
+    return invokeDsBridge('wxNativePay', params)
+  }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,30 @@
-/* global __dirname, require, module*/
+/* global __dirname, require, module */
 
-const webpack = require('webpack');
-const path = require('path');
-const env = require('yargs').argv.env; // use --env with webpack 2
-const pkg = require('./package.json');
+const webpack = require('webpack')
+const path = require('path')
+const env = require('yargs').argv.env // use --env with webpack 2
+const pkg = require('./package.json')
 
-let libraryName = pkg.name;
+let libraryName = pkg.name
 
-let outputFile, mode;
+let outputFile, mode, sourceMap
 
 if (env === 'build') {
-  mode = 'production';
-  outputFile = libraryName + '.min.js';
+  mode = 'production'
+  sourceMap = false
+  outputFile = libraryName + '.min.js'
 } else {
-  mode = 'development';
-  outputFile = libraryName + '.js';
+  mode = 'development'
+  sourceMap = 'inline-source-map'
+  outputFile = libraryName + '.js'
 }
 
 const config = {
   mode: mode,
-  entry: __dirname + '/src/index.js',
-  devtool: 'inline-source-map',
+  entry: `${__dirname}/src/index.js`,
+  devtool: sourceMap,
   output: {
-    path: __dirname + '/lib',
+    path: `${__dirname}/lib`,
     filename: outputFile,
     library: libraryName,
     libraryTarget: 'umd',
@@ -47,6 +49,6 @@ const config = {
     modules: [path.resolve('./node_modules'), path.resolve('./src')],
     extensions: ['.json', '.js']
   }
-};
+}
 
-module.exports = config;
+module.exports = config
